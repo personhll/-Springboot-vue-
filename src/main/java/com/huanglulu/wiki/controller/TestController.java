8e38b361541446e37f9fd6f15f84fa03f02008f7
huang <595188175@qq.com>
2022-02-22 15:00:02 +0800

    2-7增加hello接口测评test.http

--- src/main/java/com/huanglulu/wiki/controller/TestController.java
+++ src/main/java/com/huanglulu/wiki/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.huanglulu.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 //RestController一般用来返回字符串
 //Controller 用来返回页面
 @RestController
@@ -12,6 +16,7 @@ public class TestController {
      * GET,,POST,PUT,DELETE
      *RequestMapping支持所有
      * status= 405，浏览器直接访问地址，发送的是GET请求 ，而接口写的是POST
+     * @RequestMapping(value = "/user/1",method = RequestMethod.GET)  请求访问userid=1
      * /user?id=1
      * /user/1
      * @return
@@ -19,5 +24,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "hello world!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello world! Post,"+name;
     }
 }
