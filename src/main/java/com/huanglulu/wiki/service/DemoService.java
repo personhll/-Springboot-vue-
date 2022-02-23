8f0f50cdb0525aac21a6bb5864a5765a534cae91
huang <595188175@qq.com>
2022-02-23 19:21:40 +0800

    3-5  增加了demo表列表查询

---
+++ src/main/java/com/huanglulu/wiki/service/DemoService.java
@@ -0,0 +1,20 @@
+package com.huanglulu.wiki.service;
+
+import com.huanglulu.wiki.domain.Demo;
+import com.huanglulu.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+
+    }
+}
