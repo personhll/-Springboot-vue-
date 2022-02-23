77ec2c56ddf90f5137cbeac33664818ff01117b3
huang <595188175@qq.com>
2022-02-23 19:44:17 +0800

    3-6 增加通用的返回类CommentResp，以后所有后端接口的返回值全部都是CommonRespl类型

--- src/main/java/com/huanglulu/wiki/controller/EbookController.java
+++ src/main/java/com/huanglulu/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.huanglulu.wiki.controller;
 
 import com.huanglulu.wiki.domain.Ebook;
+import com.huanglulu.wiki.resp.CommonResp;
 import com.huanglulu.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,13 @@ public class EbookController {
 
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+
+        //在Commonresp里放一些前端需要的通用属性
+        CommonResp<List<Ebook>>resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 
 }
