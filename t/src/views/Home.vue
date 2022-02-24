93f208f83a077e4ffd3f30ef2f662c00d3e22d7e
huang <595188175@qq.com>
2022-02-24 21:49:09 +0800

    5-2 使用axios调用后端接口/ebook/list

--- t/src/views/Home.vue
+++ t/src/views/Home.vue
@@ -12,8 +12,16 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
+  setup(){
+      console.log("setup");
+      // function (response) {}相当于(response)=> {}
+      axios.get("http://localhost:8080/ebook/list?name=Spring").then((response)=> {
+          console.log(response);
+      })
+   }
 });
 </script>
