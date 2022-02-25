a059f0b7b68c3c6216030a7180c89e962f616235
huang <595188175@qq.com>
2022-02-25 10:13:41 +0800

    5-4 加入Ant Design Vue list组件

--- t/src/views/Home.vue
+++ t/src/views/Home.vue
@@ -14,6 +14,20 @@
 import { defineComponent ,onMounted,ref,reactive,toRef} from 'vue';
 import axios from 'axios';
 
+const listData: any = [];
+
+for (let i = 0; i < 23; i++) {
+    listData.push({
+        href: 'https://www.antdv.com/',
+        title: `ant design vue part ${i}`,
+        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        description:
+            'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+        content:
+            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    });
+}
+
 export default defineComponent({
   name: 'Home',
   setup(){
@@ -33,7 +47,19 @@ export default defineComponent({
       });
       return {
           ebooks,
-          ebook2:toRef(ebook1,"books")
+          ebook2:toRef(ebook1,"books"),
+          listData,
+          pagination:{
+              onChange: (page: any)=> {
+                  console.log(page);
+              },
+              pageSize: 3,
+          },
+            actions:[
+          { type: 'StarOutlined', text: '156' },
+          { type: 'LikeOutlined', text: '156' },
+          { type: 'MessageOutlined', text: '2' },
+      ],
       }
    }
 });
