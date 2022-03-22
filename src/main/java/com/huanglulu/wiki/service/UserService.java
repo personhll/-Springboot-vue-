2aa4496856414d92af7657b5d46fb2042a26711d
huang <595188175@qq.com>
2022-03-22 21:03:44 +0800

    9-4 修改时，用户名不允许修改

--- src/main/java/com/huanglulu/wiki/service/UserService.java
+++ src/main/java/com/huanglulu/wiki/service/UserService.java
@@ -87,7 +87,8 @@ public class UserService {
             }
         }else{
             //更新
-            userMapper.updateByPrimaryKey(user);
+            user.setLoginName(null);
+            userMapper.updateByPrimaryKeySelective(user);
         }
     }
 
