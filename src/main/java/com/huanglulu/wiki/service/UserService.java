5764b3060c0c44007cce7362ccf4de0f1f4343af
huang <595188175@qq.com>
2022-03-22 21:08:16 +0800

    9-4 修改时，不修改密码 v-show="!user.id"

--- src/main/java/com/huanglulu/wiki/service/UserService.java
+++ src/main/java/com/huanglulu/wiki/service/UserService.java
@@ -88,6 +88,7 @@ public class UserService {
         }else{
             //更新
             user.setLoginName(null);
+            user.setPassword(null);
             userMapper.updateByPrimaryKeySelective(user);
         }
     }
