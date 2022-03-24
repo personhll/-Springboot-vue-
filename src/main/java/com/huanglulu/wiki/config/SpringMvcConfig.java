4a6633b182baa856adce1d6ee373fb05af62f591
huang <595188175@qq.com>
2022-03-24 16:28:30 +0800

    9-11 前后端增加登录拦截 修复BUG，前后端存储token统一用String类型

--- src/main/java/com/huanglulu/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/huanglulu/wiki/config/SpringMvcConfig.java
@@ -1,38 +1,44 @@
 package com.huanglulu.wiki.config;
 
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
+import com.huanglulu.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
 //
 //    @Resource
 //    LogInterceptor logInterceptor;
-//    @Resource
-//    LoginInterceptor loginInterceptor;
-//
-//    @Resource
-//    ActionInterceptor actionInterceptor;
+    @Resource
+    LoginInterceptor loginInterceptor;
 
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(loginInterceptor)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns(
-//                        "/test/**",
-//                        "/redis/**",
-//                        "/user/login",
-//                        "/category/all",
-//                        "/ebook/list",
-//                        "/doc/all/**",
-//                        "/doc/vote/**",
-//                        "/doc/find-content/**",
-//                        "/ebook-snapshot/**"
-//                );
-//
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
+
 //        registry.addInterceptor(actionInterceptor)
 //                .addPathPatterns(
 //                        "/*/save",
 //                        "/*/delete/**",
 //                        "/*/reset-password");
-
+//
 //        registry.addInterceptor(logInterceptor)
 //                .addPathPatterns("/**").excludePathPatterns("login");
-//    }
-//}
+    }
+}
