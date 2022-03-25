caf601f367b72469dce72f9295ad5ce53f89db26
huang <595188175@qq.com>
2022-03-25 17:32:10 +0800

    10-6 首页显示电子书信息：文档数，阅读数，点赞数 doc/vote/** 点赞接口不需要登录拦截

--- t/src/views/admin/admin-doc.vue
+++ t/src/views/admin/admin-doc.vue
@@ -163,7 +163,7 @@
               level1.value = [];
               level1.value = Tool.array2Tree(docs.value,0);
               console.log("树形结构：",level1);
-              treeSelectData.value = Tool.copy(level1.value);
+              treeSelectData.value = Tool.copy(level1.value) || [];
               //为选择添加一个“无”
               treeSelectData.value.unshift({id: 0,name: '无'});
 
--- t/src/views/home.vue
+++ t/src/views/home.vue
@@ -11,110 +11,105 @@
 </template>
 
 <script lang="ts">
-import { defineComponent ,onMounted,ref,reactive,toRef} from 'vue';
-import axios from 'axios';
-import { message } from "ant-design-vue";
-import {Tool} from "@/util/tool";
+    import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
+    import axios from 'axios';
+    import {message} from "ant-design-vue";
+    import {Tool} from "@/util/tool";
 
-// const listData: any = [];
-//
-// for (let i = 0; i < 23; i++) {
-//     listData.push({
-//         href: 'https://www.antdv.com/',
-//         title: `ant design vue part ${i}`,
-//         avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-//         description:
-//             'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-//         content:
-//             'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-//     });
-// }
+    // const listData: any = [];
+    //
+    // for (let i = 0; i < 23; i++) {
+    //     listData.push({
+    //         href: 'https://www.antdv.com/',
+    //         title: `ant design vue part ${i}`,
+    //         avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    //         description:
+    //             'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    //         content:
+    //             'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    //     });
+    // }
 
-export default defineComponent({
-  name: 'Home',
-  setup(){
-      const ebooks = ref();
+    export default defineComponent({
+        name: 'Home',
+        setup() {
+            const ebooks = ref();
 
-      const  level1 = ref();
-      let categorys: any;
+            const level1 = ref();
+            let categorys: any;
 
-      /**
-       * 查询所有分类
-       */
-      const handleQueryCategory = () => {
-          axios.get("/category/all").then((response) => {
-              const data = response.data;
-              if(data.success){
-                  categorys = data.content;
-                  console.log("原始数据：",categorys);
+            /**
+             * 查询所有分类
+             */
+            const handleQueryCategory = () => {
+                axios.get("/category/all").then((response) => {
+                    const data = response.data;
+                    if (data.success) {
+                        categorys = data.content;
+                        console.log("原始数据：", categorys);
 
-                  level1.value = [];
-                  level1.value = Tool.array2Tree(categorys,0);
-                  console.log("树形结构：",level1.value);
-              }else{
-                  message.error(data.message);
-              }
-          });
-      };
+                        level1.value = [];
+                        level1.value = Tool.array2Tree(categorys, 0);
+                        console.log("树形结构：", level1.value);
+                    } else {
+                        message.error(data.message);
+                    }
+                });
+            };
 
-      const isShowWelcome = ref(true);
-      let categoryId2 = 0;
+            const isShowWelcome = ref(true);
+            let categoryId2 = 0;
 
-      const handleQueryEbook = () => {
-          axios.get("/ebook/list",{
-              params:{
-                  page: 1,
-                  size:1000,
-                  categoryId2:categoryId2
-              }
-          }).then((response)=> {
-              const data = response.data;
-              ebooks.value = data.content.list;
-              //ebook1.books =data.content;
-          });
-      };
+            const handleQueryEbook = () => {
+                axios.get("/ebook/list", {
+                    params: {
+                        page: 1,
+                        size: 1000,
+                        categoryId2: categoryId2
+                    }
+                }).then((response) => {
+                    const data = response.data;
+                    ebooks.value = data.content.list;
+                    //ebook1.books =data.content;
+                });
+            };
 
-      const handleClick = (value: any) =>{
-          console.log("menu click",value);
-          // isShowWelcome.value = value.key === 'welcome';
-          if(value.key === 'welcome'){
-              isShowWelcome.value = true;
-          }else {
-              categoryId2 = value.key;
-              isShowWelcome.value = false;
-              handleQueryEbook();
-          }
-      };
+            const handleClick = (value: any) => {
+                console.log("menu click", value);
+                // isShowWelcome.value = value.key === 'welcome';
+                if (value.key === 'welcome') {
+                    isShowWelcome.value = true;
+                } else {
+                    categoryId2 = value.key;
+                    isShowWelcome.value = false;
+                    handleQueryEbook();
+                }
+            };
 
-      onMounted(()=>{
-          // function (response) {}相当于(response)=> {}
+            onMounted(() => {
+                // function (response) {}相当于(response)=> {}
 
-          handleQueryCategory();
-          // handleQueryEbook()
+                handleQueryCategory();
+                // handleQueryEbook()
 
-      });
-      return {
-          ebooks,
-          //ebook2:toRef(ebook1,"books"),
-          //listData,
-          pagination:{
-              onChange: (page: any)=> {
-                  console.log(page);
-              },
-              pageSize: 3,
-          },
-            actions:[
-          { type: 'StarOutlined', text: '156' },
-          { type: 'LikeOutlined', text: '156' },
-          { type: 'MessageOutlined', text: '2' },
-      ],
-          handleClick,
-          level1,
+            });
+            return {
+                ebooks,
+                //ebook2:toRef(ebook1,"books"),
+                //listData,
+                pagination: {
+                    onChange: (page: any) => {
+                        console.log(page);
+                    },
+                    pageSize: 3,
+                },
+                handleClick,
+                level1,
 
-          isShowWelcome,
-      }
-   }
-});
+                isShowWelcome,
+            }
+        }
+    });
 </script>
 
 <!--只在当前页面生效-->
