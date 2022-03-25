d24aef36bc1f7eb5d905e96ad922ef2c01e06cc9
huang <595188175@qq.com>
2022-03-25 15:38:18 +0800

    10-6 增加定时器，定时执行电子书信息更新SQL

--- src/main/java/com/huanglulu/wiki/mapper/DocMapperCust.java
+++ src/main/java/com/huanglulu/wiki/mapper/DocMapperCust.java
@@ -7,4 +7,8 @@ public interface DocMapperCust {
     public void increaseViewCount(@Param("id") Long id);
 
     public void increaseVoteCount(@Param("id") Long id);
+
+    public void updateEbookInfo();
+
+
 }
