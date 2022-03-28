35a43a3571871a09976ccc0f4202bb0b4ffbb6a0
huang <595188175@qq.com>
2022-03-29 00:53:35 +0800

    11-11 致谢

--- t/src/views/about.vue
+++ t/src/views/about.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>about us</h1>
+    <a-layout class="thanks">
+      <h1>感谢各位老师四年的悉心指导，耐心教育，愿老师们万事顺遂，平安喜乐</h1>
+    </a-layout>
+
   </div>
 </template>
+<style>
+  .thanks{
+    background-color:#9999cc;
+    width: 900px;
+    height: 700px;
+    font-size: 30px;
+    font-weight: bold;
+    margin:  50px auto;
+    text-indent: 2em;
+    padding:50px 100px;
+
+  }
+</style>
