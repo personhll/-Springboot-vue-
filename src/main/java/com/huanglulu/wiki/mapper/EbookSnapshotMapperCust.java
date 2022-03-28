6846f6cddd0357c11b99fbcb9bbde4b300718598
huang <595188175@qq.com>
2022-03-28 22:02:58 +0800

    11-5 增加了定时任务，定时收集数据 要再xml里一次执行多个SQL的话，需要为数据库增加参数： &allowMultiQueries=true

---
+++ src/main/java/com/huanglulu/wiki/mapper/EbookSnapshotMapperCust.java
@@ -0,0 +1,10 @@
+package com.huanglulu.wiki.mapper;
+
+public interface EbookSnapshotMapperCust {
+
+    public void genSnapshot();
+//
+//    List<StatisticResp> getStatistic();
+//
+//    List<StatisticResp> get30Statistic();
+}
