d4664cde9e42aec7c3efa276315c72cb55b8e151
huang <595188175@qq.com>
2022-03-28 21:42:17 +0800

    11-4 电子书快照表增加唯一键，一个电子书一天只能有一条记录

--- doc/all.sql
+++ doc/all.sql
@@ -114,7 +114,7 @@ create table `user` (
 insert into `user` (id, `login_name`, `name`, `password`) values (1, 'test', '测试', 'e70e2222a9d67c4f2eae107533359aa4');
 
 
--- 电子书快照表
+-- --电子书快照表
 drop table if exists `ebook_snapshot`;
 create table `ebook_snapshot` (
                                   `id` bigint auto_increment not null comment 'id',
