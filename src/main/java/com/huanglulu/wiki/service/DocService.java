45cdfceb4332db62270127fae456ea5f3288143f
huang <595188175@qq.com>
2022-03-28 18:28:36 +0800

    10-11 使用RocketMQ解耦点赞功能，发送和接受调试成功 rocketmq本地启动： mqnamesrv.cmd mqbroker.cmd -n 127.0.0.1:9876 antoCreateTopicEnable=true 使用autoCreateTopicEnable,让客户端可以自由创建topic

--- src/main/java/com/huanglulu/wiki/service/DocService.java
+++ src/main/java/com/huanglulu/wiki/service/DocService.java
@@ -18,6 +18,7 @@ import com.huanglulu.wiki.util.CopyUtil;
 import com.huanglulu.wiki.util.RedisUtil;
 import com.huanglulu.wiki.util.RequestContext;
 import com.huanglulu.wiki.util.SnowFlake;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -50,6 +51,9 @@ public class DocService {
 
     @Resource
     public WsService wsService;
+
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
 
 
     public PageResp<DocQueryResp> list(DocQueryReq req){
@@ -158,7 +162,7 @@ public class DocService {
 //        docMapperCust.increaseVoteCount(id);
         //远程IP+doc.id作为key24小时不能重复
         String ip = RequestContext.getRemoteAddr();
-        if(redisUtil.validateRepeat("DOC_VOTE_"+id+"_"+ip,3600*24)){
+        if(redisUtil.validateRepeat("DOC_VOTE_"+id+"_"+ip,5000)){
             docMapperCust.increaseVoteCount(id);
         }else {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
@@ -167,8 +171,8 @@ public class DocService {
         //推送消息
         Doc docDb = docMapper.selectByPrimaryKey(id);
         String logId = MDC.get("LOG_ID");
-        wsService.sendInfo("【"+docDb.getName()+"】被点赞！",logId);
-
+//        wsService.sendInfo("【"+docDb.getName()+"】被点赞！",logId);
+        rocketMQTemplate.convertAndSend("VOTE_TOPIC","【"+docDb.getName()+"】被点赞！");
     }
 
     public void updateEbookInfo(){
