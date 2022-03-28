5d7fefbf4278eabc204d9059f9101160beaca720
huang <595188175@qq.com>
2022-03-29 00:37:46 +0800

    11-11 富文本框图片，视频自适应

--- t/src/views/doc.vue
+++ t/src/views/doc.vue
@@ -188,5 +188,15 @@
     font-weight: 600;
   }
 
+    /* 图片自适应 */
+    .wangeditor img {
+      max-width: 100%;
+      height: auto;
+    }
 
+    /* 视频自适应 */
+    .wangeditor iframe {
+      width: 100%;
+      height: 400px;
+    }
 </style>
